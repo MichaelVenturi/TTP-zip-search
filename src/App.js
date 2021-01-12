de7558e3590ef2db802926395ea4a5a51918aa4a
Michael Venturi <michaelventuri58@aol.com>
2021-01-12 14:37:46 -0500

    setup fetch method

--- src/App.js
+++ src/App.js
@@ -1,13 +1,42 @@
-import logo from "./logo.svg";
+import React from "react";
+import reactDOM from "react-dom";
 import "./App.css";
+import City from "./components/City";
 
 function App() {
+  function displayResults() {
+    getResults();
+    const resultDiv = document.querySelector("#results");
+
+    reactDOM.render(<City></City>, resultDiv);
+  }
+
+  const getResults = async () => {
+    const zip = document.querySelector("#zip-input").value;
+    try {
+      let response = await fetch(`http://ctp-zip-api.herokuapp.com/zip/${zip}`);
+      console.log("response", response);
+      if (!response.ok) {
+        throw new Error("ERROR");
+      }
+      let data = await response.json();
+      console.log("data", data);
+    } catch (error) {
+      console.log("error", error);
+    }
+  };
+
   return (
     <div className="app">
-      <div>
+      <div className="app-header">
         <h1>ZIP search</h1>
       </div>
-      <div></div>
+      <div>
+        <label for="zip-input">Zip Code: </label>
+        <input type="text" id="zip-input" name="zip-input"></input>
+        <button onClick={displayResults}>Search</button>
+      </div>
+      <div id="results"></div>
     </div>
   );
 }
---
+++ src/components/City.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+function City(props) {
+  return (
+    <div>
+      <div>
+        <h1>City Name</h1>
+        <ul>
+          <li>state</li>
+          <li>location</li>
+          <li>population</li>
+          <li>tota wages</li>
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default City;
